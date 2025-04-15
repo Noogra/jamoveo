67c6ffb56162ba856a707c0ae5a1497c11817606
Nir Beja <90895452+Noogra@users.noreply.github.com>
2025-04-15 12:07:12 +0300

    Add login route and password validation

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -31,10 +31,41 @@ router.post('/register', async (req, res) => {
         res.status(201).json({ message: 'User registered successfully' });
     } catch(err) {
         console.error(err);
-        res.status(500).json({ message: 'Server error' });
+        res.status(500).json({ message: 'Server error creating a new user' });
+    }
+});
+
+// login user
+router.post('/login', async (req, res) => {
+    try {
+        // get user login data 
+        const { username, password } = req.body;
+
+        // check if there is such username
+        const user = await User.findOne({ username });
+        if(!user) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+
+        // check if password is correct
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(401).json({ message: 'Invalid username or password' });
+        }
+        res.status(200).json({ message: 'User login successful',
+            user: {
+                id: user._id,
+                username: user.username,
+                instrument: user.instrument,
+                role: user.role
+              }
+         });        
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error login to user' });
     }
 });
 
 
 
-module.exports = router
+module.exports = router;
