05bb756cc7650a90ad9d488c02565edaade233c6
Nir Beja <90895452+Noogra@users.noreply.github.com>
2025-04-17 12:09:16 +0300

    Fix socket.io origin domain

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const app = require("./app")
 const http = require("http")
+const mongoose = require("mongoose")
 const { Server } = require("socket.io")
 const PORT = process.env.PORT || 5050
-const mongoose = require("mongoose")
 
 require("dotenv").config()
 
@@ -12,7 +12,7 @@ const server = http.createServer(app)
 // connect to socket.io
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000", // frontend address
+    origin: "https://jamoveo-eight.vercel.app", // frontend address
     methods: ["GET", "POST"],
   },
 })
@@ -48,10 +48,3 @@ mongoose
   .catch((err) => {
     console.error("MongoDB connection error:", err)
   })
-
-console.log("MONGO_URI:", process.env.MONGO_URI)
-
-// run server
-// server.listen(PORT, () => {
-//   console.log(`Server running with WebSocket on port ${PORT}`)
-// })
